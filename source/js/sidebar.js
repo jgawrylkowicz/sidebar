3e2e632ed2154368efe19c8c79d0a4016666480a
Jakub Gawrylkowicz <jgawrylkowicz@gmail.com>
2018-02-24 11:42:19 +0100

    reworked posts structure (grid), changed the topbar behaviour

--- source/js/sidebar.js
+++ source/js/sidebar.js
@@ -21,7 +21,7 @@
   }).ready(function() {
     //console.log('rendered');
   }).on('pjax:end', function(){
-    resize_excerpts();
+    //resize_excerpts();
   });
 })(jQuery);
 
@@ -44,36 +44,6 @@ $('body,html').click(function(event){
 
 // Flowtype.js
 $('body').flowtype({
-  minFont : 12,
-  maxFont : 17
+   minFont : 12,
+   maxFont : 15
 });
-
-
-// Resize the posts' excerpt 
-// There seems to be no other way
-$( window ).resize(function(){
-  resize_excerpts();
-});
-
-//TODO doesnt work with pjax
-var resize_excerpts = function(){
-  let post_height = $('.post_container').height();
-  let content_height = $('.post_thumbnail').height() + $('.post_content_thumbnail').height() + $('.post_footer').height();
-  let height = (post_height - content_height) * .8;
-  $('.post_excerpt_hidden').css('height' , height)
-  console.log("resized", height);
-}
-
-window.onscroll = function(){
-  let topbar = $('#topbar');
-  if ($(window).scrollTop() > topbar.height()){
-    topbar.addClass('show-topbar');
-  } else {
-    topbar.removeClass('show-topbar');
-  }
-}
-
-// Functions loaded at the beginning
-window.onload = function(){
-  resize_excerpts();
-};
